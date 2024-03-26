ed8da3930ae1a05e9fa4cb4b602acdf2937ceed4
ThomasWGrant <157439361+ThomasWGrant@users.noreply.github.com>
2024-03-25 22:21:34 -0400

    Tile map and Jumping

---
+++ 2.5_degrees_unity_game/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI; // Added this line
+
+public class GameTimer : MonoBehaviour {
+    public int timer = 0;
+    private float theTimer = 0f;
+    public GameObject timerText;
+
+    void FixedUpdate(){
+        theTimer += 0.01f;
+        if (theTimer >= 1f){
+            timer +=1;
+            theTimer = 0;
+            UpdateTimer();
+        }
+    }
+
+    void UpdateTimer(){
+        Text timeTextTemp = timerText.GetComponent<Text>();
+        timeTextTemp.text = "" + timer;
+    }
+}
