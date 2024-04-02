4bd4f92c1bce5f0f0bb3cd413c23672a250b69d5
ThomasWGrant <157439361+ThomasWGrant@users.noreply.github.com>
2024-04-02 09:35:19 -0400

    updates

---
+++ 2.5_degrees_unity_game/Assets/Scripts/pickUp.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour{
+
+      public GameHandler gameHandler;
+      //public playerVFX playerPowerupVFX; 
+      public bool isHealthPickUp = true;
+
+      public int healthBoost = 5;
+    
+
+      void Start(){
+            gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+            //playerPowerupVFX = GameObject.FindWithTag("Player").GetComponent<playerVFX>(); 
+      }
+
+      public void OnTriggerEnter2D (Collider2D other){ 
+            if (other.gameObject.tag == "Player"){ 
+                  GetComponent<Collider2D>().enabled = false; 
+                  GetComponent< AudioSource>().Play(); 
+                  StartCoroutine(DestroyThis());
+
+                  if (isHealthPickUp == true) {
+                        gameHandler.playerGetHit(healthBoost * -1);
+                        //playerPowerupVFX.powerup();
+                  }
+
+                  
+            }
+      } 
+
+      IEnumerator DestroyThis(){
+            yield return new WaitForSeconds(0.3f);
+            Destroy(gameObject);
+      }
+
+}
