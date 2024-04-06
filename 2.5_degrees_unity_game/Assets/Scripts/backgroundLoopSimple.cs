979a8e7e1983f63ed2a49fd7fe17b0551a4dce7d
janeschmelkin <jane.schmelkin@tufts.edu>
2024-04-06 10:46:23 -0400

    finally fixed background

--- 2.5_degrees_unity_game/Assets/Scripts/backgroundLoopSimple.cs
+++ 2.5_degrees_unity_game/Assets/Scripts/backgroundLoopSimple.cs
@@ -6,13 +6,14 @@ public class backgroundLoopSimple : MonoBehaviour {
     //  private Transform centerBG;
       [SerializeField] private Transform centerBG;
       public float offset = 12;       //this value is the width of the image
+      public GameObject player;
 
       void Update(){
-            if (transform.position.x >= centerBG.position.x + offset){
-                  centerBG.position = new Vector2(transform.position.x + offset, centerBG.position.y);
+            if (player.transform.position.x >= centerBG.position.x + offset){
+                  centerBG.position = new Vector2(player.transform.position.x + offset, centerBG.position.y);
             }
-            else if (transform.position.x <= centerBG.position.x - offset){
-                  centerBG.position = new Vector2(transform.position.x - offset, centerBG.position.y);
+            else if (player.transform.position.x <= centerBG.position.x - offset){
+                  centerBG.position = new Vector2(player.transform.position.x - offset, centerBG.position.y);
             }
       }
 }
